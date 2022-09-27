21916b783c03345904416eee26dbdf96974ecefc
carambola <celestebaltazar@yahoo.com>
2022-09-26 20:19:51 -0700

    finished thoughts controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -9,7 +9,7 @@ const thoughtController = {
 },
 
     getThoughtById (req, res) {
-    Thought.findOne({ _id: req.params.userId })
+    Thought.findOne({_id: params.id })
     .select('-__v')
     .then((Thought) =>
       !Thought
@@ -22,6 +22,17 @@ const thoughtController = {
 createThought(req, res) {
     Thought.create(req.body)
       .then((Thought) => res.json(Thought))
+      .catch((err) => res.status(500).json(err));
+  },
+
+deleteCourse(req, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((Thought) =>
+        !Thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : Thought.deleteMany({ _id: { $in: Thought.id } })
+      )
+      .then(() => res.json({ message: 'Thought deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
 

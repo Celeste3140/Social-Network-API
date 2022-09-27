70424c653c00a656edce1eb36c1a9d5d633ff1ba
carambola <celestebaltazar@yahoo.com>
2022-09-26 21:49:58 -0700

    finished user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,17 +4,35 @@ const userController = {
 
     getAllUsers(req, res) {
         User.find({})
-            .then((User) => res.json(User))
+            .then((User) => res.json(dbUser))
             .catch((err) => res.status(500).json(err));
     },
-    getUsersById(req, res) {
+
+    getUserById(req, res) {
         Users.findOne({ _id: params.id })
             .select('-__v')
-            .then((dbUsers) =>
-                !Thought
+            .then((dbUser) =>
+                !dbUser
                     ? res.status(404).json({ message: 'No Users with that ID' })
                     : res.json(dbUser)
             )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+            .then((dbUser) => res.json(dbUser))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    deleteCourse(req, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUser => {
+                if (!dbUser) {
+                    return res.status(404).json ({ message: 'No user found with this id.'})
+                }
+            })
+            .then(() => res.json({ message: 'user deleted!' }))
             .catch((err) => res.status(500).json(err));
     },
 };
